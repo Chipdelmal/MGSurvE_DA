c377e341fa49ca2dda6c059b1316cdff269a6af2
chipdelmal <chipdelmal@gmail.com>
2023-05-27 08:06:22 -0700

    BFA Scraping update

--- Africa/BFA/lndLandScrape.sh
+++ Africa/BFA/lndLandScrape.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-python lndLandWeb.py 'sami' 'Burkina Faso' 'BFA' 'Manga' '11.6679, -1.0760' '2000' '0.0225' '3'
-python lndMigrationMatrix.py 'sami' 'Burkina Faso' 'BFA' 'Manga' '11.6679, -1.0760'
+LG='\033[1;34m'
+NC='\033[0m'
 
+echo -e "${LG}* Clustering DBSCAN${NC}"
 python lndLandWeb.py 'sami' 'Burkina Faso' 'BFA' 'Fanka' '13.1490, -1.0171' '2500' '0.0175' '3'
-python lndMigrationMatrix.py 'sami' 'Burkina Faso' 'BFA' 'Fanka' '13.1490, -1.0171' '2500'
-
+python lndLandWeb.py 'sami' 'Burkina Faso' 'BFA' 'Manga' '11.6679, -1.0760' '2000' '0.0225' '3'
 python lndLandWeb.py 'sami' 'Burkina Faso' 'BFA' 'Niangoloko' '10.2826803, -4.9240132' '4500' '0.02' '5'
-python lndMigrationMatrix.py 'sami' 'Burkina Faso' 'BFA' 'Niangoloko' '10.2826803, -4.9240132'
-
 python lndLandWeb.py 'sami' 'Burkina Faso' 'BFA' 'Nouna' '12.7326, -3.8603' '5000' '0.018' '3'
-python lndMigrationMatrix.py 'sami' 'Burkina Faso' 'BFA' 'Nouna' '12.7326, -3.8603'
-
 python lndLandWeb.py 'sami' 'Burkina Faso' 'BFA' 'Banfora' '10.6376, -4.7526' '4000' '0.0175' '3'
+
+echo -e "${LG}* Calculating Migration${NC}"
+python lndMigrationMatrix.py 'sami' 'Burkina Faso' 'BFA' 'Fanka' '13.1490, -1.0171'
+python lndMigrationMatrix.py 'sami' 'Burkina Faso' 'BFA' 'Manga' '11.6679, -1.0760'
+python lndMigrationMatrix.py 'sami' 'Burkina Faso' 'BFA' 'Niangoloko' '10.2826803, -4.9240132'
+python lndMigrationMatrix.py 'sami' 'Burkina Faso' 'BFA' 'Nouna' '12.7326, -3.8603'
 python lndMigrationMatrix.py 'sami' 'Burkina Faso' 'BFA' 'Banfora' '10.6376, -4.7526'
+
+
 
 # python lndLandWeb.py 'sami' 'Burkina Faso' 'BFA' 'Toece' '11.8302, -1.2656' '1000'
 # python lndLandWeb.py 'sami' 'Burkina Faso' 'BFA' 'Fada NGourma' '12.0627, 0.3695' '2000'
