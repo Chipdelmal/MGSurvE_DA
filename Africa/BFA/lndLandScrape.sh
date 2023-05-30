fec00d8f633ef010acecf6abc081ef6448fcbd6d
chipdelmal <chipdelmal@gmail.com>
2023-05-30 10:19:54 -0700

    BFA Major improvement to bash script

--- Africa/BFA/lndLandScrape.sh
+++ Africa/BFA/lndLandScrape.sh
@@ -2,19 +2,41 @@
 
 USR=$1
 
+###############################################################################
+# Auxiliary terminal constants
+###############################################################################
 LG='\033[1;34m'
+RD="\033[1;31m"
 NC='\033[0m'
 
-echo -e "${LG}* Clustering DBSCAN${NC}"
-python lndLandWeb.py $USR 'Burkina Faso' 'BFA' 'Fanka' '13.1490, -1.0171' '2500' '0.0175' '3'
-python lndLandWeb.py $USR 'Burkina Faso' 'BFA' 'Manga' '11.6679, -1.0760' '2000' '0.0225' '3'
-python lndLandWeb.py $USR 'Burkina Faso' 'BFA' 'Niangoloko' '10.2826803, -4.9240132' '4500' '0.02' '5'
-python lndLandWeb.py $USR 'Burkina Faso' 'BFA' 'Nouna' '12.7326, -3.8603' '5000' '0.018' '3'
-python lndLandWeb.py $USR 'Burkina Faso' 'BFA' 'Banfora' '10.6376, -4.7526' '4000' '0.0175' '3'
+###############################################################################
+# Declare cities to loop through
+###############################################################################
+declare -a cities
+cities[0]='Fanka;13.1490, -1.0171;2500;0.0175;3'
+cities[1]='Manga;11.6679, -1.0760;2000;0.0225;3'
+cities[2]='Niangoloko;10.2826803, -4.9240132;4500;0.02;5'
+cities[3]='Nouna;12.7326, -3.8603;5000;0.018;3'
+cities[4]='Banfora;10.6376, -4.7526;4000;0.0175;3'
 
-echo -e "${LG}* Calculating Migration${NC}"
-python lndMigrationMatrix.py $USR 'Burkina Faso' 'BFA' 'Fanka' '13.1490, -1.0171'
-python lndMigrationMatrix.py $USR 'Burkina Faso' 'BFA' 'Manga' '11.6679, -1.0760'
-python lndMigrationMatrix.py $USR 'Burkina Faso' 'BFA' 'Niangoloko' '10.2826803, -4.9240132'
-python lndMigrationMatrix.py $USR 'Burkina Faso' 'BFA' 'Nouna' '12.7326, -3.8603'
-python lndMigrationMatrix.py $USR 'Burkina Faso' 'BFA' 'Banfora' '10.6376, -4.7526'
+###############################################################################
+# Loop through cities
+###############################################################################
+for city in "${cities[@]}"
+do
+    # Split elements of array -------------------------------------------------
+    IFS=";" read -r -a arr <<< "${city}"
+    name="${arr[0]}"
+    lonlat="${arr[1]}"
+    distance="${arr[2]}"
+    eps="${arr[3]}"
+    neighbors="${arr[4]}"
+    echo -e "${RD}* Processing $name...${NC}"
+    # Launch scripts ----------------------------------------------------------
+    echo -e "${LG}\t* Clustering...${NC}"
+    python lndLandWeb.py $USR 'Burkina Faso' 'BFA'\
+        "$name" "$lonlat" "$distance" "$eps" "$neighbors"
+    echo -e "${LG}\t* Aggregating...${NC}"
+    python lndMigrationMatrix.py $USR 'Burkina Faso' 'BFA'\
+        "$name" "$lonlat"
+done
