38323de79571ef765a6fe5feaf104172c02ff528
chipdelmal <chipdelmal@gmail.com>
2023-08-21 11:28:06 -0700

    STC Code and paths improvements

--- Benchmarks/STP/STC_PreProcessMatrices.py
+++ Benchmarks/STP/STC_PreProcessMatrices.py
@@ -16,58 +16,42 @@ from sys import argv
 import numpy as np
 import pandas as pd
 from os import path
-from sys import argv
-from copy import deepcopy
+import compress_pickle as pkl
 import matplotlib.pyplot as plt
-from numpy.random import uniform
 from sklearn.preprocessing import normalize
 import MGSurvE as srv
-import cartopy.crs as ccrs
+import constants as cst
+pd.options.mode.chained_assignment = None 
 
 
 ###############################################################################
 # Bash and user inputs
 ###############################################################################
 if srv.isNotebook():
-    # User input (interactive session)
-    (FXD_TRPS, AP, TRPS_NUM, RID) = (True, 'man', 5, '07')
+    pass
 else:
-    # Bash call input
-    (FXD_TRPS, AP, TRPS_NUM, RID) = (True, 'man', int(argv[1]), int(argv[2]))
-ID = 'STC'
-OUT_PTH = './sims_out/'
+    pass
+(ID, OUT_PTH) = (cst.EXP_ID, cst.PATHS['data'])
 ###############################################################################
 # Not accessible masses
 ###############################################################################
-IX_SPLIT = 27
-NOT_ACCESSIBLE = [51, 239]
+(IX_SPLIT, NOT_ACCESSIBLE) = (cst.IX_SPLIT, cst.NOT_ACCESSIBLE)
 NOT_ACCESSIBLE_IX = tuple([i-IX_SPLIT for i in NOT_ACCESSIBLE])
 ###############################################################################
 # Output Folder
 ###############################################################################
-RID = int(RID)
 srv.makeFolder(OUT_PTH)
-###############################################################################
-# Debugging fixed traps at land masses
-###############################################################################
-ID = 'STP' if FXD_TRPS else 'STPN'
 ###############################################################################
 # Load Pointset
 ###############################################################################
-sites = pd.read_csv(path.join('./GEO', 'STP_LatLonN.csv'))
+sites = pd.read_csv(path.join(cst.PATHS['geo'], 'STP_LatLonN.csv'))
 sites['t'] = [0]*sites.shape[0]
 SAO_TOME_LL = sites.iloc[IX_SPLIT:]
-SAO_bbox = (
-    (min(SAO_TOME_LL['lon']), max(SAO_TOME_LL['lon'])),
-    (min(SAO_TOME_LL['lat']), max(SAO_TOME_LL['lat']))
-)
-SAO_cntr = [i[0]+(i[1]-i[0])/2 for i in SAO_bbox]
-SAO_LIMITS = ((6.41, 6.79), (-0.0475, .45))
 ###############################################################################
 # Load Migration Matrix
 ###############################################################################
 migration = np.genfromtxt(
-    path.join('./GEO', 'STP_MigrationN.csv'), 
+    path.join(cst.PATHS['geo'], 'STP_MigrationN.csv'), 
     delimiter=','
 )
 msplit = migration[IX_SPLIT:,IX_SPLIT:]
@@ -78,3 +62,16 @@ SAO_TOME_LL.drop(index=NOT_ACCESSIBLE, axis=0, inplace=True)
 msplit = np.delete(msplit, NOT_ACCESSIBLE_IX, axis=0)
 msplit = np.delete(msplit, NOT_ACCESSIBLE_IX, axis=1)
 SAO_TOME_MIG = normalize(msplit, axis=1, norm='l1')
+###############################################################################
+# Calculate distance matrix
+###############################################################################
+migDst = srv.calcDistanceMatrix(
+    np.asarray(SAO_TOME_LL[['lon', 'lat']]), 
+    distFun=srv.haversineDistance
+)
+###############################################################################
+# Dump files to disk
+###############################################################################
+pkl.dump(migDst, path.join(OUT_PTH, f'{ID}-DST'), compression='bz2')
+pkl.dump(SAO_TOME_MIG, path.join(OUT_PTH, f'{ID}-MIG'), compression='bz2')
+pkl.dump(SAO_TOME_LL, path.join(OUT_PTH, f'{ID}-POS'), compression='bz2')
---
+++ Benchmarks/STP/constants.py
@@ -0,0 +1,10 @@
+
+
+(IX_SPLIT, NOT_ACCESSIBLE) = (27, [51, 239])
+SAO_LIMITS = ((6.41, 6.79), (-0.0475, .45))
+EXP_ID = 'STC'
+PATHS = {
+    'data': './DTA', 
+    'geo': './GEO', 
+    'optimization': './OPT'
+}
