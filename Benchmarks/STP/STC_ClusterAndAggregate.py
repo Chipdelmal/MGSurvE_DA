f0b3dffbaa454cc2bdd5879a769580d6307970b0
chipdelmal <chipdelmal@gmail.com>
2023-08-21 14:13:49 -0700

    STC Bash script for clustering

--- Benchmarks/STP/STC_ClusterAndAggregate.py
+++ Benchmarks/STP/STC_ClusterAndAggregate.py
@@ -19,9 +19,9 @@ pd.options.mode.chained_assignment = None
 # Bash and user inputs
 ###############################################################################
 if srv.isNotebook():
-    (EPS, MIN) = (500, 1)
+    (EPS, MIN) = (1250, 1)
 else:
-    pass
+    (EPS, MIN) = (int(argv[1]), 1)
 (ID, PTH_IN, PTH_OUT) = (
     cst.EXP_ID,
     cst.PATHS['data'], cst.PATHS['data']
@@ -54,7 +54,7 @@ aggMat = monet.aggregateLandscape(MIG, PTS['cluster'], type=0)
 ###############################################################################
 # Export to Disk
 ###############################################################################
-fID = f'{ID}-{EPS:05d}'
+fID = f'{ID}-{clustersNum:03d}'
 pkl.dump(aggMat, path.join(PTH_OUT, fID+'-AGG'), compression='bz2')
 pkl.dump(PTS, path.join(PTH_OUT, fID+'-CLS'), compression='bz2')
 ###############################################################################
@@ -65,3 +65,7 @@ colors = [pal[i%len(pal)] for i in PTS['cluster']]
 (fig, ax) = plt.subplots(figsize=(SZE, SZE))
 ax.scatter(PTS['lon'], PTS['lat'], color=colors)
 ax.set_aspect('equal')
+fig.savefig(
+    path.join(PTH_OUT, fID+'-AGG'), 
+    bbox_inches='tight', pad_inches=0, dpi=DPI, transparent=False
+)
